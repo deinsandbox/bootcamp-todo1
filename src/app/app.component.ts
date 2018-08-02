2df43cbc957e8ac9c8c891e3598d3850a08d0250
equiman <equiman@users.noreply.github.com>
2018-08-01 22:00:45 -0500

    Add person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  name: string;
+  age: number;
+  user: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BASE';
+  title: string = 'App';
+
+  person: Person = {
+    name: 'Camilo Martinez',
+    age: 38,
+    user: 'equiman',
+  };
+
+  constructor() {
+    // this.person.name;
+    // this.person.age;
+    // this.person.user;
+  }
 }
